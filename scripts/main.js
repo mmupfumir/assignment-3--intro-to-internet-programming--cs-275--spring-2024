166156579bcc479dc3de1cf4cea1edbcee3c94ea
rw555 <157328874+rw555@users.noreply.github.com>
2024-04-20 17:56:11 -0400

    added nav id change

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,14 @@
 let isMobileSized = false;
+let nav = document.getElementsByTagName(`nav`)[0];
 
 window.addEventListener(`load`, () => {
     if (736 < window.innerWidth) {
         isMobileSized = false;
+        nav.setAttribute(`id`, `drop-down`);
     }
     else {
         isMobileSized = true;
+        nav.setAttribute(`id`, `side-tray`);
     }
     console.log(isMobileSized);
     console.log(`Loaded`);
