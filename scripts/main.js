0692e8caa4bc6b5295c13309444d308e4bd02e04
jayang <wotjd5101@gmail.com>
2024-04-21 00:34:04 -0400

    create drop down menu
    create drop-down menu

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,19 @@
-let MenuAnchor1 = document.querySelector(`nav > ul > li:nth-child(1) > a`);
-let MenuAnchor2 = document.querySelector(`nav > ul > li:nth-child(1) > a`);
-let ulMenu1 = document.querySelector(`nav > ul > li:nth-child(1) > ul`);
-let ulMenu2 = document.querySelector(`nav > ul > li:nth-child(2) > ul`);
+document.addEventListener(`DOMContentLoaded`, () => {
+    const showMenuTrigger = document.querySelector(`#js-triggers li:first-child a`);
+    const nav = document.querySelector(`nav`);
 
-MenuAnchor1.setAttribute(`class`, `dropdown-toggle`);
-MenuAnchor2.setAttribute(`class`, `dropdown-toggle`);
-ulMenu1.setAttribute(`class`, `dropdown-menu`);
-ulMenu2.setAttribute(`class`, `dropdown-menu`);
+    showMenuTrigger.addEventListener(`click`, (event) => {
+        event.preventDefault();
+        nav.classList.toggle(`visible`);
+    });
+
+    const menuItems = document.querySelectorAll(`nav ul > li > a`);
+
+    menuItems.forEach((menuItem) => {
+        menuItem.addEventListener(`click`, (event) => {
+            event.preventDefault();
+            const subMenu = menuItem.nextElementSibling;
+            subMenu.classList.toggle(`visible`);
+        });
+    });
+});
