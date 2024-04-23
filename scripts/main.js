f552069cfaf0ce903eb4ed7141ca96648810b3cc
jayang <wotjd5101@gmail.com>
2024-04-23 15:52:26 -0400

    Create side-tray

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,26 @@
+const body = document.querySelector(`body`);
+const bodySideTrayHTML = `
+<nav class="side-tray">
+    <ul>
+      <li><a href="#">Menu 1</a>
+        <ul>
+          <li><a href="#">1.1</a></li>
+          <li><a href="#">1.2</a></li>
+          <li><a href="#">1.3</a></li>
+        </ul>
+      </li>
+      <li><a href="#">Menu 2</a>
+        <ul>
+          <li><a href="#">2.1</a></li>
+          <li><a href="#">2.2</a></li>
+          <li><a href="#">2.3</a></li>
+        </ul>
+      </li>
+    </ul>
+</nav>`;
+
+body.innerHTML += bodySideTrayHTML;
+
 document.addEventListener(`DOMContentLoaded`, () => {
     const showMenuTrigger = document.querySelector(`#js-triggers li:first-child a`);
     const nav = document.querySelector(`nav`);
@@ -5,7 +28,6 @@ document.addEventListener(`DOMContentLoaded`, () => {
 
     showMenuTrigger.addEventListener(`click`, (event) => {
         event.preventDefault();
-
         if (window.innerWidth <= 735) {
             sideTray.classList.toggle(`visible`);
         } else {
@@ -15,12 +37,25 @@ document.addEventListener(`DOMContentLoaded`, () => {
     });
 
     const menuItems = document.querySelectorAll(`nav ul > li > a`);
+    const menuItemsSideTray = document.querySelectorAll(`.side-tray > ul > li > ul`);
 
     menuItems.forEach((menuItem) => {
         menuItem.addEventListener(`click`, (event) => {
             event.preventDefault();
-            const subMenu = menuItem.nextElementSibling;
-            subMenu.classList.toggle(`visible`);
+            const subMenuDropDown = menuItem.nextElementSibling;
+            subMenuDropDown.classList.toggle(`visible`);
+        });
+    });
+
+    menuItemsSideTray.forEach((menuItemSideTray) => {
+        menuItemSideTray.addEventListener(`click`, (event) => {
+            event.preventDefault();
+            const subMenu = menuItemSideTray.nextElementSibling;
+            if (subMenu) {
+                console.log(`Inside of if statement`);
+                console.log(subMenu.classList);
+                subMenu.classList.toggle(`visible`);
+            }
         });
     });
 });
