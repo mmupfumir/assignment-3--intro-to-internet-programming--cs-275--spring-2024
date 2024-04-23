1d9b12d3af7becaa08bb68bf1d6e5b138f8610d5
wpit63 <73858845+wpit63@users.noreply.github.com>
2024-04-23 16:40:58 -0400

    changed to arrow function

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,8 @@
-const menu1 = document.getElementsByClassName(`menu`)[0];
-const menu2 = document.getElementsByClassName(`menu`)[1];
 const menuButton = document.getElementById(`menu-button`);
 const menuNav = document.getElementById(`menu-nav`);
 let visible = false;
 
-menuButton.onclick = function () {
+menuButton.addEventListener(`click`, () => {
     if (!visible) {
         menuNav.style.visibility = `visible`;
         visible = true;
@@ -12,4 +10,4 @@ menuButton.onclick = function () {
         menuNav.style.visibility = `hidden`;
         visible = false;
     }
-};
+});
