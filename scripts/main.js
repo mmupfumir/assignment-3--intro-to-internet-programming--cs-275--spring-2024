200c8cea5faab60495dd026c45705390d2162598
brynova <bryant321e@gmail.com>
2024-04-24 12:14:29 -0400

    Added Id's to specify menu sections

--- scripts/main.js
+++ scripts/main.js
@@ -2,3 +2,4 @@ heading = document.querySelector('header');
 navigation = document.querySelector('nav');
 unorderedList = document.querySelector('ul');
 jsTriggers = document.querySelector('js-triggers');
+show = document.querySelector('')
